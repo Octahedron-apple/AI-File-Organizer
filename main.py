5e4ddc546df51225b4ed883c18235f94ac6aa0a7
Octahedron-apple <bhavya1singh1@gmail.com>
2026-03-28 00:53:48 +0530

    Added code to list all files in a directory

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 import ollama
 import os
-PATH="./"
+PATH=""
 with open("categories.txt","r") as f:
     categories=f.readlines()
+
+files = []
+
+for root, dirs, a in os.walk(PATH):
+    for i in a:
+        files.append(os.path.join(root, i))
+        print(len(files))
+print(files)
